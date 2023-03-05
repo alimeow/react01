97077e0ca5b6e799dce2e99ed538eb7dc15cf929
Kojiro <kojiroest@gmail.com>
2023-03-04 22:08:02 -0800

    temp

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+
 const Content = () => {
+  const[items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    },
+    {
+        id: 2,
+        checked: false,
+        item: "Item 2"
+    },
+    {
+        id: 3,
+        checked: false,
+        item: "Item 3"
+    }
+  ]);
+
+  const handleClick = (e) => {
+    console.log(items)
+    console.log(typeof items)
+  }
+
   return (
     <main>
-      <h3>content</h3>
+      <div>
+        {/* <button onClick={handleClick}>click</button> */}
+        <ul>
+          {items.map(x => <li key={x.id}>{x.item}</li>)}
+        </ul>
+      </div>
     </main>
   )
 };
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,7 @@
 const Header = () => {
-  const headerstyle = {
-    backgroundColor: 'royalblue',
-    color: '#fff'
-  }
 
   return (
-    <header style={headerstyle}>
+    <header>
       <h1>Groceries List</h1>
     </header>
   )
