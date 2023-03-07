3794a4fd486192f54aa7767aee1438ea7cba2c35
Kojiro <kojiroest@gmail.com>
2023-03-06 22:21:34 -0800

    temp

---
+++ src/AddItem.js
@@ -0,0 +1,22 @@
+import { FaPlus } from 'react-icons/fa';
+
+const AddItem = ({length}) => {
+  return (
+    <form className='addForm' >
+      <label htmlFor='addItem'>Add Item</label>
+      <input
+        autoFocus
+        id='addItem'
+        type='text'
+        placeholder='Add Item'
+        required
+      />
+      <button
+        type='submit'
+        aria-label='Add Item'
+      ><FaPlus /></button>
+    </form>
+  )
+};
+
+export default AddItem;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './index.css';
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
@@ -42,6 +43,7 @@ function App() {
   return (
     <div className="App">
       <Header title="Grocery List" />
+      <AddItem />
       <Content
       items={items}
       toggleCheck={toggleCheck}
